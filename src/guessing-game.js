15708639b490a12677a9c901bdf15ea4ea8e3ae5
Uglekiska <uglekiska@yandex.by>
2021-09-02 21:05:02 +0300

    fix: update guessing-game file

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,29 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        //this.mid = Math.floor((this.min + this.max)/2);
+        // this.min = min;
+        // this.max = max;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        //this.mid = Math.floor((this.min + this.max)/2);
+        this.mid = Math.ceil((this.min + this.max)/2);
+        return this.mid;
     }
 
     lower() {
-
+        this.max = this.guess();
+        //this.setRange(this.min, this.max);
     }
 
     greater() {
-
+        this.min = this.guess();
+        //this.setRange(this.min, this.max);
     }
 }
 
